982621f33867a02247197969298e038c52c0a830
spacemanOG <107798246+SpacemanOG@users.noreply.github.com>
2023-01-07 22:00:20 +0600

    Python practice from book TSTP

---
+++ book_tstp_challenges_ch4.py
@@ -0,0 +1,14 @@
+# problem 1
+def myFunc(x):
+    return x ** 2
+
+
+print(myFunc(15))
+
+# problem 2
+
+
+def myFunc2(stringParam):
+    print(str(stringParam))
+
+myFunc2("Hello")
---
+++ main.py
@@ -0,0 +1,5 @@
+def myFunc(x):
+    return x**2
+
+
+print(myFunc(10))
