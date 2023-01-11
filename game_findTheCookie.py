b298ee15153dae8b8d09978339eea684aa869f0e
spacemanOG <107798246+SpacemanOG@users.noreply.github.com>
2023-01-11 21:15:57 +0600

    Create game_findTheCookie.py

---
+++ game_findTheCookie.py
@@ -0,0 +1,10 @@
+print("Welcome to the game called >>> Where's the cookie?")
+print("|_1_||_2_||_3_||_4_||_5_|")
+print("The cookie is hidden behind one of those doors numbered from 1 to 5.")
+print("To win, you have to guess the correct number.")
+print("You get 3 chances.")
+
+input("To begin, write >>> Yes ")
+
+player_name = input("What's your name? ")
+print("Hello,", player_name)
