822ade72abd908d66056a10f3576552e51231768
spacemanOG <107798246+SpacemanOG@users.noreply.github.com>
2023-01-11 20:56:37 +0600

    Create book_tstp_ch5.py

---
+++ book_tstp_ch5.py
@@ -0,0 +1,13 @@
+# Create a list of your favorite musicians
+
+fav_musicians = ["weeknd", "drake", "porcupine tree", "metallica", "eminem"]
+
+print(fav_musicians)
+
+# Create a list of tuples,with each tuple containing the longitude and latitude of somewhere you've lived or visited
+
+my_list_of_tuples = [(23.57019, 89.84970), (23.74452, 90.40152)]
+
+print(my_list_of_tuples)
+
+
